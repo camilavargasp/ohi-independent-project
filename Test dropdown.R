6653e2f2311b7fe96485d1c503e243b51b572ac5
camilavargasp <cvargas@bren.ucsb.edu>
2018-07-31 14:54:04 -0700

    All files

---
+++ Test dropdown.R
@@ -0,0 +1,39 @@
+
+
+library(plotly)
+
+x <- seq(-2 * pi, 2 * pi, length.out = 1000)
+df <- data.frame(x, y1 = sin(x), y2 = cos(x)) #"x"  "y1" "y2"
+
+p <- plot_ly(df, x = ~x) %>%
+  add_lines(y = ~y1, name = "A") %>%
+  add_lines(y = ~y2, name = "B", visible = F) %>%
+  layout(
+    title = "Drop down menus - Styling",
+    xaxis = list(domain = c(0.1, 1)),
+    yaxis = list(title = "y"),
+    updatemenus = list(
+      list(
+        y = 0.8,
+        buttons = list(
+          
+          list(method = "restyle",
+               args = list("line.color", "blue"),
+               label = "Blue"),
+          
+          list(method = "restyle",
+               args = list("line.color", "red"),
+               label = "Red"))),
+      
+      list(
+        y = 0.7,
+        buttons = list(
+          list(method = "restyle",
+               args = list("visible", list(TRUE, FALSE)),
+               label = "Sin"),
+          
+          list(method = "restyle",
+               args = list("visible", list(FALSE, TRUE)),
+               label = "Cos")))
+    )
+  )
